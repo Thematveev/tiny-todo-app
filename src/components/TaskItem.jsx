a706db99c8f8fcc370b579251b2f3d11a8c10a31
Maksim Matveev <thematveev21@gmail.com>
2024-06-27 21:33:04 +0300

    some changes

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -3,12 +3,14 @@ import styled from "styled-components"
 const Item = styled.li`
     padding: 15px 20px;
     border: 1px solid gainsboro;
+    display: flex;
+    gap: 10px;
 `
 
 export default function TaskItem({index, text}){
     return (
         <Item>
-            <span>{index}</span>
+            <span>#{index}</span>
             <span>{text}</span>
         </Item>
     )
